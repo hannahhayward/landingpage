c9223ebc47ad0fcbe1d7ab17bdb9b0e1e455b29c
hannah hayward <hannahhayward97@gmail.com>
2021-08-09 19:40:38 -0600

    updated data in state

--- src/AppState.js
+++ src/AppState.js
@@ -8,28 +8,38 @@ export const AppState = reactive({
     {
       name: 'Keeper',
       img: 'https://images-platform.99static.com/IPG0oX6KOQrFeVzKkgcRibkpqF4=/157x157:1417x1417/500x500/top/smart/99designs-contests-attachments/115/115998/attachment_115998183',
-      type: 'social networking application'
+      type: 'social networking application',
+      description: 'A full stack social networking platform',
+      tech: 'HTML5, CSS, Vue3, Bootstrap, JavaScript ES6, C#, MySQL & .NET'
     },
     {
       name: 'The Network',
       img: 'https://e7.pngegg.com/pngimages/524/455/png-clipart-computer-icons-mesh-networking-share-icon-others-miscellaneous-computer-network.png',
-      type: 'social networking application'
+      type: 'social networking platform',
+      description: 'A full stack social networking platform',
+      tech: 'HTML5, JavaScript ES6, Vue3, CSS, Node.js, Bootstrap & .NET'
     },
     {
       name: 'Bug Log',
       img: 'https://www.pinclipart.com/picdir/middle/499-4998717_ants-on-a-log-clipart-png-download.png',
-      type: 'tracking application'
+      type: 'tracking application',
+      description: 'A full stack forum application to solve and track software bugs.',
+      tech: 'HTML5, JavaScript ES6, CSS, Vue3, Node.js, Bootstrap & .NET'
     }],
   collaborations: [
     {
       name: 'Hogout',
       img: 'https://github.com/EricWakeman/hogout/blob/master/hogout.client/src/assets/img/hogoutTitle.png',
-      type: 'social platform'
+      type: 'social platform',
+      description: 'Collaborated with a team of four to develop a full stack social networking platform.',
+      tech: 'JavaScript ES6, Vue3, CSS, HTML5, Bootstrap, Node.js & .NET'
     },
     {
       name: 'KanBan',
       img: 'https://github.com/wyattDockstader/kanban/blob/master/kanban%20logo.png?raw=true',
-      type: 'project management application'
+      type: 'project management application',
+      description: 'Collaborated with a partner developer to develop a full stack KanBan board application.',
+      tech: 'JavaScript ES6, Vue3, CSS, HTML5, Bootstrap, Node.js & .NET'
     }
   ],
   contacts: [
